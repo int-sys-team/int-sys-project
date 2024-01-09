cc98985d00fef6fb611190b3579dc864abd40d84
unknown <aleksamilic2224@gmail.com>
2024-01-09 01:25:07 +0100

    paging implemented

--- Austin-Estates/vite-project/src/HouseDashboard/RentalDashboard.tsx
+++ Austin-Estates/vite-project/src/HouseDashboard/RentalDashboard.tsx
@@ -4,7 +4,12 @@ import { CssVarsProvider } from '@mui/joy/styles';
 import CssBaseline from '@mui/joy/CssBaseline';
 import Box from '@mui/joy/Box';
 import Stack from '@mui/joy/Stack';
-
+import IconButton, { iconButtonClasses } from '@mui/joy/IconButton';
+import Typography from '@mui/joy/Typography';
+import ArrowBackIosRoundedIcon from '@mui/icons-material/ArrowBackIosRounded';
+import ArrowForwardIosRoundedIcon from '@mui/icons-material/ArrowForwardIosRounded';
+import BoxJ from '@mui/joy/Box';
+import ButtonJ from '@mui/joy/Button';
 import RentalCard from './RentalCard';
 import HeaderSection from './HeaderSection';
 import Search from './Search';
@@ -17,6 +22,105 @@ import PropertyMap from './PropertyMap';
 import { getProperties } from '../api/properties';
 import { AI_API_URL, API_URL } from '../utils/config';
 
+interface PaginationProps {
+	currentPage: number;
+	totalPages: number;
+	onNextPage: () => void;
+	onPrevPage: () => void;
+	setCurrentPage: (page: number) => void;
+   }
+   
+   function Pagination({ currentPage, totalPages, onNextPage, onPrevPage, setCurrentPage }: PaginationProps) {
+	return (
+	  <div>
+		<BoxJ
+		  className="Pagination-mobile"
+		  sx={{
+			display: { xs: 'flex', md: 'none' },
+			alignItems: 'center',
+			mx: 2,
+			my: 1,
+		  }}
+		>
+		  <IconButton
+			aria-label="previous page"
+			variant="outlined"
+			color="neutral"
+			size="sm"
+			onClick={onPrevPage}
+			disabled={currentPage === 1}
+		  >
+			<ArrowBackIosRoundedIcon />
+		  </IconButton>
+		  <Typography level="body-sm" mx="auto">
+			Page {currentPage} of {totalPages}
+		  </Typography>
+		  <IconButton
+			aria-label="next page"
+			variant="outlined"
+			color="neutral"
+			size="sm"
+			onClick={onNextPage}
+			disabled={currentPage === totalPages}
+		  >
+			<ArrowForwardIosRoundedIcon />
+		  </IconButton>
+		</BoxJ>
+		<BoxJ
+		  className="Pagination-laptopUp"
+		  sx={{
+			gap: 1,
+			[`& .${iconButtonClasses.root}`]: { borderRadius: '50%' },
+			display: {
+			  xs: 'none',
+			  md: 'flex',
+			},
+			mx: 4,
+			my: 2,
+		  }}
+		>
+		  <ButtonJ
+			size="sm"
+			variant="plain"
+			color="neutral"
+			startDecorator={<ArrowBackIosRoundedIcon />}
+			onClick={onPrevPage}
+			disabled={currentPage === 1}
+		  >
+			Previous
+		  </ButtonJ>
+   
+		  <BoxJ sx={{ flex: 1 }} />
+   
+		  {Array.from({ length: totalPages }, (_, i) => i).map((page) => (
+			<IconButton
+			  key={page}
+			  size="sm"
+			  variant={page === currentPage ? 'soft' : 'outlined'}
+			  color="neutral"
+			  onClick={() => setCurrentPage(page)}
+			>
+			  {page+1}
+			</IconButton>
+		  ))}
+   
+		  <BoxJ sx={{ flex: 1 }} />
+   
+		  <ButtonJ
+			size="sm"
+			variant="plain"
+			color="neutral"
+			endDecorator={<ArrowForwardIosRoundedIcon />}
+			onClick={onNextPage}
+			disabled={currentPage === totalPages}
+		  >
+			Next
+		  </ButtonJ>
+		</BoxJ>
+	  </div>
+	);
+   }
+
 export default function RentalDashboard(props) {
 	const { category, title, rareFind = false, liked = false, image } = props;
 	const navigate = useNavigate();
@@ -24,19 +128,40 @@ export default function RentalDashboard(props) {
 	const [properties, setProperties] = useState<any[]>([]);
 	const [loading, setLoading] = useState<boolean>(false);
 
+	const [currentPage, setCurrentPage] = useState(1);
+ 	const itemsPerPage = 3;
+
+	 const [displayedProperties, setDisplayedProperties] = useState<any[]>([]);
+
+
+	const nextPage = () => {
+		setCurrentPage((prevPage) => prevPage + 1);
+	   };
+	  
+	const prevPage = () => {
+		setCurrentPage((prevPage) => prevPage - 1);
+	   };
+
+	useEffect(() => {
+		setDisplayedProperties(properties.slice((currentPage - 1) * itemsPerPage, currentPage * itemsPerPage));
+	}, [currentPage]);
+
 	useEffect(() => {
 		try {
-			setLoading(true);
-			getProperties().then((data) => {
-				console.log(data);
-				setProperties(data.properties);
-			});
+		   setLoading(true);
+		   const startIndex = currentPage * itemsPerPage;
+		   //const endIndex = startIndex + itemsPerPage;
+		   getProperties().then((data) => {
+			   console.log(data);
+			   setProperties(data.properties); 
+			   setDisplayedProperties(data.properties.slice(startIndex, startIndex + 3)); //koliko zelimo item-a po page-u
+		   });
 		} catch (e) {
-			console.log(e);
+		   console.log(e);
 		} finally {
-			setLoading(false);
+		   setLoading(false);
 		}
-	}, []);
+	   }, [currentPage]);
 
 	const naturalQueryProperties = async (query: string) => {
 		try {
@@ -75,7 +200,7 @@ export default function RentalDashboard(props) {
 			<Box
 				component="main"
 				sx={{
-					height: 'calc(100vh - 55px)', // 55px is the height of the NavBar
+					height: 'calc(95vh - 55px)', // 55px is the height of the NavBar
 					display: 'grid',
 					gridTemplateColumns: { xs: 'auto', md: '60% 40%' },
 					gridTemplateRows: 'auto 1fr auto',
@@ -141,12 +266,19 @@ export default function RentalDashboard(props) {
 						</Box>
 					) : (
 						<Stack spacing={2} sx={{ overflow: 'auto' }}>
-							{properties.map((property) => (
+							{displayedProperties.map((property) => (
 								<RentalCard key={property._id} {...property} />
 							))}
 						</Stack>
 					)}
 				</Stack>
+				<Pagination
+					currentPage={currentPage}
+					totalPages={Math.ceil(properties.length / itemsPerPage)}
+					onNextPage={nextPage}
+					onPrevPage={prevPage}
+					setCurrentPage={setCurrentPage}
+					/>
 			</Box>
 		</CssVarsProvider>
 	);
